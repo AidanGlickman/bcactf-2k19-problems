30d2f3b4f9dd9c88384ae05fc83e1b82f7823c0b
Anthony Li <li.anthony917@gmail.com>
2019-01-02 19:36:34 -0500

    Support multiple cookies

--- web/cookie-clicker/source-code/static/cookie-clicker.js
+++ web/cookie-clicker/source-code/static/cookie-clicker.js
@@ -2,7 +2,7 @@ window.addEventListener("load", e => {
   // Lets store cookies as HTTP cookies!
   // Because they're cookies... see what I did there?
 
-  const cookieCookie = document.cookie && document.cookie.split(";").map(cookie => cookie.split("=")).find(cookie => cookie[0] === "cookies");
+  const cookieCookie = document.cookie && document.cookie.split(";").map(cookie => cookie.split("=")).find(cookie => cookie[0].trim() === "cookies");
   cookies = cookieCookie && cookieCookie.length > 1 ? parseInt(cookieCookie[1]) : 0;
 
   const updateCookies = () => {
@@ -11,7 +11,7 @@ window.addEventListener("load", e => {
   updateCookies();
 
   document.querySelector(".cookie").addEventListener("click", e => {
-    const cookieCookie = document.cookie && document.cookie.split(";").map(cookie => cookie.split("=")).find(cookie => cookie[0] === "cookies");
+    const cookieCookie = document.cookie && document.cookie.split(";").map(cookie => cookie.split("=")).find(cookie => cookie[0].trim() === "cookies");
     if (cookieCookie && cookieCookie.length > 1 && !isNaN(parseInt(cookieCookie[1]))) {
       cookies = cookieCookie[1];
     }
